427c3d9bf896a9c12d4e99cc13b66b476a0ef4e3
nkmess <junna.m_21@icloud.com>
2019-06-16 14:18:06 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,7 +44,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/mercari/pem']
+                  keys: ['~/.ssh/mercari.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
